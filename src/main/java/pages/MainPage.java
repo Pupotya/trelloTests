54bce5ec74de26ce0538a585f671787f9db2f905
Max <xpro100bestx@gmail.com>
2018-02-19 18:15:22 +0200

    hotfix, return to the previous version

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -4,12 +4,12 @@ import core.MethodsFactory;
 
 public class MainPage extends MethodsFactory {
 
-    private String pageUrl = "https://trello.com/login";
-
     public LoginForm loginForm = new LoginForm();
 
     public void open() {
-        driver().get(pageUrl);
+        driver().get(getProp("baseUrl"));
     }
+
+
 
 }
